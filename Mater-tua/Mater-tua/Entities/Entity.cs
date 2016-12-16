7d88af8db33497bb0b2607cf4226c4b53c1104f0
Rachel <rachelbroek92@gmail.com>
2016-12-16 19:05:52 +0100

    Added AnimatedEntity(empty) and Unit classes

--- Mater-tua/Mater-tua/Entities/Entity.cs
+++ Mater-tua/Mater-tua/Entities/Entity.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Graphics;
 public class Entity
 {
 
-    protected Vector2 _position, _velocity;
+    protected Vector2 _position;
     protected int _layer;
 
     public Entity(int layer = 0)
@@ -14,27 +14,15 @@ public class Entity
         Reset();
     }
 
-    public virtual void Update(GameTime gameTime)
-    {
-        _position += _velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
-    }
-
     public virtual void Reset()
     {
         _position = Vector2.Zero;
-        _velocity = Vector2.Zero;
     }
 
     public virtual Vector2 Position
     {
         get { return _position; }
         set { _position = value; }
-    }
-
-    public virtual Vector2 Velocity
-    {
-        get { return _velocity; }
-        set { _velocity = value; }
     }
 
     public virtual int Layer

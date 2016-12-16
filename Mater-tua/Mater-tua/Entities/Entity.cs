f276e7cca0ea5d64b1a4b189333251e157633f51
Rachel <rachelbroek92@gmail.com>
2016-12-16 18:04:30 +0100

    Reviewed & adjusted Entity and SpriteEntity classes.

--- Mater-tua/Mater-tua/Entities/Entity.cs
+++ Mater-tua/Mater-tua/Entities/Entity.cs
@@ -5,22 +5,13 @@ using Microsoft.Xna.Framework.Graphics;
 public class Entity
 {
 
-    protected Entity _parent;
     protected Vector2 _position, _velocity;
-    protected string _id;
-    protected bool _visible;
     protected int _layer;
 
-    public Entity(int layer = 0, string id = "")
+    public Entity(int layer = 0)
     {
         this._layer = layer;
-        this._id = id;
         Reset();
-    }
-
-    public virtual void HandleInput(InputHelper inputHelper)
-    {
-
     }
 
     public virtual void Update(GameTime gameTime)
@@ -28,14 +19,8 @@ public class Entity
         _position += _velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
     }
 
-    public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
-    {
-
-    }
-
     public virtual void Reset()
     {
-        _visible = true;
         _position = Vector2.Zero;
         _velocity = Vector2.Zero;
     }
@@ -58,68 +43,6 @@ public class Entity
         set { _layer = value; }
     }
 
-    public bool Visible
-    {
-        get { return _visible; }
-        set { _visible = value; }
-    }
-
-    public virtual Entity Parent
-    {
-        get { return _parent; }
-        set { _parent = value; }
-    }
-
-    public string Id
-    {
-        get { return _id; }
-    }
-
-    public virtual Vector2 GlobalPosition
-    {
-        get
-        {
-            if(_parent != null)
-            {
-                return _parent.GlobalPosition + Position;
-            }
-            else
-            {
-                return Position;
-            }
-        }
-    }
-
-    public Entity Root
-    {
-        get
-        {
-            if(_parent != null)
-            {
-                return _parent.Root;
-            }
-            else
-            {
-                return this;
-            }
-        }
-    }
-    /*
-    public Entity GameWorld
-    {
-        get
-        {
-            return Root as EntityList
-        }
-    } */
-
-    public virtual Rectangle BoundingBox
-    {
-        get
-        {
-            return new Rectangle((int)GlobalPosition.X, (int)GlobalPosition.Y, 0, 0);
-        }
-    }
 }
 
 
--- Mater-tua/Mater-tua/Entities/SpriteEntity.cs
+++ Mater-tua/Mater-tua/Entities/SpriteEntity.cs
@@ -10,10 +10,9 @@ public class SpriteEntity : Entity
 {
     protected SpriteSheet _sprite;
     protected Vector2 _origin;
-   // public bool PerPixelCollisionDetection = true;
 
-    public SpriteEntity(string assetName, int layer = 0, string id = "", int sheetIndex = 0)
-        : base(layer, id)
+    public SpriteEntity(string assetName, int layer = 0,  int sheetIndex = 0)
+        : base(layer)
     {
         if (assetName != "")
         {
@@ -25,13 +24,13 @@ public class SpriteEntity : Entity
         }
     }
 
-    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
-        if (!Visible || _sprite == null)
+        if (_sprite == null)
         {
             return;
         }
-        _sprite.Draw(spriteBatch, this.GlobalPosition, _origin);
+        _sprite.Draw(spriteBatch, this.Position, _origin);
     }
 
     public SpriteSheet Sprite
@@ -54,11 +53,6 @@ public class SpriteEntity : Entity
         get { return _sprite.Height; }
     }
 
-    public bool Mirror
-    {
-        get { return _sprite.Mirror; }
-        set { _sprite.Mirror = value; }
-    }
 
     public Vector2 Origin
     {
@@ -66,42 +60,15 @@ public class SpriteEntity : Entity
         set { _origin = value; }
     }
 
-    public override Rectangle BoundingBox
+    public Rectangle BoundingBox
     {
         get
         {
-            int left = (int)(GlobalPosition.X - _origin.X);
-            int top = (int)(GlobalPosition.Y - _origin.Y);
+            int left = (int)(Position.X - _origin.X);
+            int top = (int)(Position.Y - _origin.Y);
             return new Rectangle(left, top, Width, Height);
         }
     }
-
-/*    public bool CollidesWith(SpriteEntity obj)
-    {
-        if (!Visible || !obj.Visible || !BoundingBox.Intersects(obj.BoundingBox))
-        {
-            return false;
-        }
-        if (!PerPixelCollisionDetection)
-        {
-            return true;
-        }
-        Rectangle b = Collision.Intersection(BoundingBox, obj.BoundingBox);
-        for (int x = 0; x < b.Width; x++)
-        {
-            int thisx = b.X - (int)(GlobalPosition.X - _origin.X) + x;
-            int thisy = b.Y - (int)(GlobalPosition.Y - _origin.Y) + y;
-            int objx = b.X - (int)(obj.GlobalPosition.X - obj._origin.X) + x;
-            int objy = b.Y - (int)(obj.GlobalPosition.Y - obj._origin.Y) + y;
-            if (_sprite.IsTranslucent(thisx, thisy) && obj._sprite.IsTranslucent(objx, objy))
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-*/
 
 }
 

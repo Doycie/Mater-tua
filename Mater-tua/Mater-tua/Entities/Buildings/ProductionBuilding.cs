997194b3b7c95770f2b6b54e2eaad990740a98f7
Jelte Friso Knossen <j.f.knossen@students.uu.nl>
2016-12-17 16:17:49 +0100

    StaticBuildings and Prodction buildings

---
+++ Mater-tua/Mater-tua/Entities/Buildings/ProductionBuilding.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class ProductionBuilding : StaticBuilding
+{
+    public ProductionBuilding(string assetName, int layer = 0) : base(assetName, layer)
+    {
+        base._BuildTime = 1000;
+        base._faction = faction.Human;
+        base._goldCost = 500;
+        base._lumberCost = 500;
+        base._hp = 1000;
+    }
+}
+
---
+++ Mater-tua/Mater-tua/Entities/Buildings/StaticBuilding.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+class StaticBuilding : SpriteEntity
+{
+    protected int _hp;
+    protected float _BuildTime;
+    protected int _lumberCost;
+    protected int _goldCost;
+    protected int[] _buildingSize;
+    protected bool _ableToFire;
+    protected bool _ableToProduce;
+
+    public enum faction { Orc, Human, Neutral }
+    protected faction _faction;
+
+    protected Texture2D _tex;
+
+    public StaticBuilding(string assetName, int layer = 0) : base(assetName, layer)
+    {
+    }
+
+    public bool AbleToProduce
+    {
+        get { return _ableToProduce; }
+    }
+
+    public bool AbleToFire
+    {
+        get { return _ableToFire; }
+    }
+
+    public int[] BuildingSize
+    {
+        get { return _buildingSize; }
+    }
+
+    public int LumberCost
+    {
+        get { return _lumberCost; }
+    }
+
+    public int GoldCost
+    {
+        get { return _goldCost; }
+    }
+
+    public int HitPoints
+    {
+        get { return _hp; }
+        set { _hp = value; }
+    }
+
+    public faction Faction
+    {
+        get { return _faction; }
+    }
+
+    public float BuildTime
+    {
+        get { return _BuildTime; }
+        set { _BuildTime = value; }
+    }
+
+    
+}
+

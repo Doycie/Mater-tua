bc453715eafdc38325d85625afca8a3bbc9a1f5a
QuintenSteve <quinten.steve@gmail.com>
2017-01-09 14:42:09 +0100

    Player class toegevoegd In WorkerUnit variable aangepast om verwarring te verkomen

---
+++ Mater-tua/Mater-tua/Player.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+class Player
+{
+    private int _Wood;
+    private int _Gold;
+
+    public Player()
+    { 
+            
+    }
+
+    public int Wood
+    {
+        get { return _Wood; }
+    }
+
+    public int Gold
+    {
+        get { return _Gold; }
+    }
+
+    public void AddWood(int Amount)
+    {
+        _Wood = _Wood + Amount;
+    }
+
+    public void AddGold(int Amount)
+    {
+        _Gold = _Gold + Amount;
+    }
+
+
+
+}
+

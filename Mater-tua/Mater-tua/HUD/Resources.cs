cae44c879815b37f10123ef6fdf0b0b6374c51cf
Justin de Boer <jutudubu@gmail.com>
2017-01-10 11:47:39 +0100

    *Added foodsprite (temp?) *Started resource class for showing resources in HUD. WIP

---
+++ Mater-tua/Mater-tua/HUD/Resources.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+class Resources
+{
+    private int _resourceNumber;
+    private Texture2D _resourceTex;
+    private int _foodCount;
+
+    public int getNumber()
+    {
+        return _resourceNumber;
+    }
+
+    public void setNumber(int number)
+    {
+        _resourceNumber = number;
+    }
+
+    public Resources(int number, Texture2D resourceTex)
+    {
+        _resourceTex = resourceTex;
+        _resourceNumber = number;
+    }
+}

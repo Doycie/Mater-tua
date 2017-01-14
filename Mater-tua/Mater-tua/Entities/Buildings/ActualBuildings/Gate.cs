6b3905d747c00c91aa9743872b6320ca46b27f0a
Jelte <j.f.knossen@students.uu.nl>
2017-01-14 12:17:31 +0100

    More building clases

---
+++ Mater-tua/Mater-tua/Entities/Buildings/ActualBuildings/Gate.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+internal class Gate : StaticBuilding
+{
+    public Gate(Level level, Vector2 position, faction faction) : base(level)
+    {
+        _EnemycanWalktrough = false;
+        _Friendcanwalktrough = true;
+        _size = 2;
+        _position = position;
+        _faction = faction;
+        _maxhp = 2500;
+        _lumberCost = 200;
+        _goldCost = 0;
+        _buildTime = 500;
+        _armor = 10;
+        _ableToProduce = true;
+        this.Reset();
+
+        if (_faction == faction.Human)
+        {
+            _description = "This is a Gate.";
+            _sprite = GameEnvironment.getAssetManager().GetSprite("");
+        }
+
+        if (_faction == faction.Orc)
+        {
+            _description = "Gate this is";
+            _sprite = GameEnvironment.getAssetManager().GetSprite("");
+        }
+    }
+    public override void Reset()
+    {
+        _hp = _maxhp;
+    }
+
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        base.Draw(spriteBatch);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}

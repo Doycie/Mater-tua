b650a2f8977d8d338c695371e38deeef21d8f562
Rachel <rachelbroek92@gmail.com>
2017-01-22 17:11:37 +0100

    FogOfWar slight optimisation

--- Mater-tua/Mater-tua/FogOfWar.cs
+++ Mater-tua/Mater-tua/FogOfWar.cs
@@ -37,30 +37,28 @@ public class FogOfWar
         //Get the view so we dont do unneccssary drawing
         Rectangle bounds = GameEnvironment.getCamera().getView();
         //Adjust the view slightly to see everything
-        bounds.X -= data.tSize();
-        bounds.Y -= data.tSize();
-        bounds.Width += data.tSize();
-        bounds.Height += data.tSize();
+        bounds.X = bounds.X / data.tSize() ;
+        bounds.Y = bounds.Y / data.tSize() ;
+        bounds.Width = bounds.Width / data.tSize() + 1;
+        bounds.Height = bounds.Height / data.tSize() + 1;
         // Console.WriteLine("X: " + bounds.X + " Y: "  + bounds.Y + " Z: " + bounds.Width + " W: " + bounds.Height);
 
         //Draw all the tiles
-        for (int x = 0; x < _fog.GetLength(0); x++)
+        for (int x = bounds.X; x < bounds.Width; x++)
         {
-            for (int y = 0; y < _fog.GetLength(1); y++)
+            for (int y = bounds.Y; y < bounds.Height; y++)
             {
-                if (bounds.Contains(x * data.tSize(), y * data.tSize()))
 
-                {
-                    if (_fog[x, y] == 0)  //if this tile has never been explored
-                        s.Draw(_fogFull, new Vector2(x * data.tSize(), y * data.tSize()), Color.White);
-                    else if (_fog[x, y] == 1) //if this tile has been explored but is no longer in vision
-                        s.Draw(_fogHalf, new Vector2(x * data.tSize(), y * data.tSize()), Color.White);
-                    //s.Draw(_tex, new Rectangle(i * 64, j * 64, i * 64 + 64, j * 64 + 64), getColor(_mapData[i, j]));
+                if (_fog[x, y] == 0)  //if this tile has never been explored
+                    s.Draw(_fogFull, new Vector2(x * data.tSize(), y * data.tSize()), Color.White);
+                else if (_fog[x, y] == 1) //if this tile has been explored but is no longer in vision
+                    s.Draw(_fogHalf, new Vector2(x * data.tSize(), y * data.tSize()), Color.White);
+                //s.Draw(_tex, new Rectangle(i * 64, j * 64, i * 64 + 64, j * 64 + 64), getColor(_mapData[i, j]));
 
-                }
             }
         }
     }
+
 
     public void Update()
     {
@@ -72,13 +70,13 @@ public class FogOfWar
                 {
                     if (e.Faction == BuildingAndUnit.faction.Human && _fog[x, y] != 3)
                     {
-                        if (Math.Abs(e.Position.X / 64 - x) <= 5 && Math.Abs(e.Position.Y / 64 - y) <= 5)
+                        if (Math.Abs(e.Position.X / 64 - x) + Math.Abs(e.Position.Y / 64 - y) <= 5)
                         {
-                            if (Distance(new Point((int)(x * data.tSize()), (int)(y * data.tSize())), new Point((int)e.Position.X, (int)e.Position.Y)) < 5 * data.tSize())
-                            {// if the distance between the entity and the tile looked at is less than 5
+                            //if (Distance(new Point((int)(x * data.tSize()), (int)(y * data.tSize())), new Point((int)e.Position.X, (int)e.Position.Y)) < 5 * data.tSize())
+                            //{// if the distance between the entity and the tile looked at is less than 5
                                 _fog[x, y] = 3; //fog 3 = fully visible since this frame, fog 2 = fully visible, fog 1 = half visible (explored but no vision currently), fog 0 = no vision (unexplored)
                                 continue;
-                            }
+                            //}
                         }
                         else
                         {
@@ -130,6 +128,6 @@ public class FogOfWar
     {
         return Math.Sqrt(Math.Pow(x.X - y.X, 2) + Math.Pow(x.Y - y.Y, 2));
     }
-
 }
+
 

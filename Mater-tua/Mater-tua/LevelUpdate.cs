4e9aa1e1f230047646870da4bd107e713390f09b
QuintenSteve <quinten.steve@gmail.com>
2017-01-23 14:06:31 +0100

    Revert "WorkerUnits worden nu door orcs aangevallen"
    This reverts commit 3252427ac20c68b0bea306974d4826418301e522.

--- Mater-tua/Mater-tua/LevelUpdate.cs
+++ Mater-tua/Mater-tua/LevelUpdate.cs
@@ -33,19 +33,6 @@ partial class Level
                     }
                 }
             }
-            foreach (Unit e in entities.OfType<Unit>())
-            {
-                if (e.Faction == Unit.faction.Human)
-                {
-                    foreach (CombatUnit g in entities.OfType<CombatUnit>())
-                    {
-                        if (g.Faction == CombatUnit.faction.Orc)
-                        {
-                            g.Defend(e);                            
-                        }
-                    }
-                }
-            }
         }
 
         
